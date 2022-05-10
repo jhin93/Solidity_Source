2847d0fa741a26e8068d8b7c99e2abb9ab2fd0b0
jhin93 <scene1993@gmail.com>
2022-05-10 22:22:26 +0900

    헬퍼함수 완료 + 이벤트리스너에 적용(프론트엔드-스마트컨트랙트)

--- dApp/dApp_0_greeting/src/App.js
+++ dApp/dApp_0_greeting/src/App.js
@@ -10,27 +10,62 @@ import './App.css';
 const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+  // Property Variables
+
+  const [message, setMessage] = useState("");
 
   // Helper Function (interact with smart contract)
 
   // Request access to the user's Meta Mask Account
   // https://metamask.io
   async function requestAccount() {
-    await window.ethereum.request( { method: 'eth_requestAccounts' } )
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
   }
 
   // Fetches the current value store in greeting
   async function fetchGreeting() {
     // If MetaMask exists
-    if(typeof window.ethereum !== "undefined"){
+    if (typeof window.ethereum !== "undefined") {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
       try {
+        // Call Greeter.greet()
+        /*
+          function greet() public view returns (string memory) {
+              return greeting;
+          }
+        */
         const data = await contract.greet();
         console.log("data : ", data);
       } catch (error) {
         console.log('Error: ', error);
       }
+    }
+  }
+
+  async function setGreeting() {
+    if (!message) return;
+
+    // If MetaMask exists
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      // Create contract with signer
+      /*
+        function setGreeting(string memory _greeting) public {
+          console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+          greeting = _greeting;
+        }
+      */
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(message);
+
+      setMessage("");
+      await transaction.wait();
+      fetchGreeting();
     }
   }
 
@@ -44,11 +79,25 @@ function App() {
         </div>
         {/* BUTTONS - Fetch and Set */}
         <div className="custom-buttons">
-          <button style={{ backgroundColor : 'green' }}>Fetch Greeting</button>
-          <button style={{ backgroundColor : 'red' }}>Set Greeting</button>
+          <button 
+          
+          // onclick-listener
+          onClick={fetchGreeting}
+
+          style={{ backgroundColor: 'green' }}>Fetch Greeting</button>
+          <button 
+          
+          // onclick-listener
+          onClick={setGreeting}
+
+          style={{ backgroundColor: 'red' }}>Set Greeting</button>
         </div>
         {/* INPUT TEXT - String */}
-        <input placeholder='Set Greeting Message' />
+        <input
+          // input-listener
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          placeholder='Set Greeting Message' />
       </div>
     </div>
   );
