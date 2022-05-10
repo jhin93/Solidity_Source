06821d1dce675d2f0582590ff1d04e345ebd01fb
jhin93 <scene1993@gmail.com>
2022-05-10 22:01:20 +0900

    헬퍼함수들 (프론트엔드 - 스마트컨트랙트)

--- dApp/dApp_0_greeting/src/App.js
+++ dApp/dApp_0_greeting/src/App.js
@@ -1,23 +1,55 @@
+import { useState } from 'react';
+import { ethers } from 'ethers'
+
+// Import ABI Code to interact with smart contract
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import logo from './logo.svg';
 import './App.css';
 
+// The contract address
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 function App() {
+
+  // Helper Function (interact with smart contract)
+
+  // Request access to the user's Meta Mask Account
+  // https://metamask.io
+  async function requestAccount() {
+    await window.ethereum.request( { method: 'eth_requestAccounts' } )
+  }
+
+  // Fetches the current value store in greeting
+  async function fetchGreeting() {
+    // If MetaMask exists
+    if(typeof window.ethereum !== "undefined"){
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet();
+        console.log("data : ", data);
+      } catch (error) {
+        console.log('Error: ', error);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'>
+        {/* DESCRIPTION */}
+        <div className="description">
+          <h1>Greeter.sol</h1>
+          <h3>Full stack dapp using ReactJS and Hardhat</h3>
+        </div>
+        {/* BUTTONS - Fetch and Set */}
+        <div className="custom-buttons">
+          <button style={{ backgroundColor : 'green' }}>Fetch Greeting</button>
+          <button style={{ backgroundColor : 'red' }}>Set Greeting</button>
+        </div>
+        {/* INPUT TEXT - String */}
+        <input placeholder='Set Greeting Message' />
+      </div>
     </div>
   );
 }
