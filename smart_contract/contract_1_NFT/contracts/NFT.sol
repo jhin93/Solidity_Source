68358ccaecbf36cf7cf631f9a493356fdd18afba
jhin93 <scene1993@gmail.com>
2022-05-11 23:29:07 +0900

    hardhat 컴파일(npx hardhat compile)

--- smart_contract/contract_1_NFT/contracts/NFT.sol
+++ smart_contract/contract_1_NFT/contracts/NFT.sol
@@ -1,9 +1,10 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract NFT is ERC1155 {
-    constructor() {
+    constructor() ERC1155("URL") {
         _mint(msg.sender, 0, 1, "");
     }
 }
