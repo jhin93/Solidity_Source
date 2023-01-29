db9616857ebddd54053ee31a424765a95e3e5686
jhin93 <scene1993@gmail.com>
2023-01-29 19:09:57 +0900

    줄바꿈 실행, 컴파일 완료

--- DAO/dao_template_practice/contracts/governance_standard/GovernorContract.sol
+++ DAO/dao_template_practice/contracts/governance_standard/GovernorContract.sol
@@ -9,7 +9,14 @@ import "../../node_modules/@openzeppelin/contracts/governance/extensions/Governo
 import "../../node_modules/@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 
 
-contract GovernorContract is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl {
+contract GovernorContract is 
+    Governor, 
+    GovernorSettings, 
+    GovernorCountingSimple, 
+    GovernorVotes, 
+    GovernorVotesQuorumFraction, 
+    GovernorTimelockControl 
+{
     constructor(IVotes _token, TimelockController _timelock, uint256 _votingDelay, uint256 _votingPeriod, uint256 _quorumPercentage)
         Governor("GovernorContract")
         GovernorSettings(_votingDelay /* 1 block */, _votingPeriod /* 50400 blocks ~ 1 week */, 0)
