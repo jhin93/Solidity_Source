4bb9e86ac0ae721ecb442be627983ab509489aba
jhin93 <scene1993@gmail.com>
2023-01-29 18:06:02 +0900

    2개파일 + GovernorContract을 Contracts Wizard에서 가져옴

---
+++ DAO/dao_template_practice/contracts/governance_standard/TimeLock.sol
@@ -0,0 +1,5 @@
+// We want to wait for a new vote to be "excuted"
+
+// Everyone who holds the governance token has to pay 5 tokens
+
+// Give time to users to "get out" if they don't like a governance update
