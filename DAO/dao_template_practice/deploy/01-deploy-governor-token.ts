d069617428c7e8aed5582e0da6a567a8ff7e6448
jhin93 <scene1993@gmail.com>
2023-01-31 17:49:59 +0900

    governance token deploy 스크립트(ts) 작성 완료

--- DAO/dao_template_practice/deploy/01-deploy-governor-token.ts
+++ DAO/dao_template_practice/deploy/01-deploy-governor-token.ts
@@ -3,10 +3,19 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from 'hardhat-deploy/types';
 
 
-const deployGovernanceToken: DeployFunction = async function (
-    hre: HardhatRuntimeEnvironment
-) {
-    console.log("Hello!")
+const deployGovernanceToken: DeployFunction = async function (  hre: HardhatRuntimeEnvironment
+) { 
+    const { getNamedAccounts, deployments, network } = hre;
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    log("Deploying Governance Token...");
+    const governanceToken = await deploy("GovernanceToken", {
+        from: deployer,
+        args: [],
+        log: true,
+        // waitConfirmations:
+    })
+    log(`Deployed governance token to address ${governanceToken.address}`);
 }
 
 export default deployGovernanceToken;
