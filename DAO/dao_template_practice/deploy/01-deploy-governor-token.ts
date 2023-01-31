a6fe44fd71892cb0c7d466e66bd5c02f9a205aea
jhin93 <scene1993@gmail.com>
2023-01-31 20:55:37 +0900

    01-deploy-governor-token.ts 완성

--- DAO/dao_template_practice/deploy/01-deploy-governor-token.ts
+++ DAO/dao_template_practice/deploy/01-deploy-governor-token.ts
@@ -1,10 +1,10 @@
 // these are the two main things you need to create a deploy function with hardhat deploy.
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers } from "hardhat";
 
 
-const deployGovernanceToken: DeployFunction = async function (  hre: HardhatRuntimeEnvironment
-) { 
+const deployGovernanceToken: DeployFunction = async function (  hre: HardhatRuntimeEnvironment) { 
     const { getNamedAccounts, deployments, network } = hre;
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
@@ -16,6 +16,26 @@ const deployGovernanceToken: DeployFunction = async function (  hre: HardhatRunt
         // waitConfirmations:
     })
     log(`Deployed governance token to address ${governanceToken.address}`);
+
+    await delegate(governanceToken.address, deployer)
+    log("Delegated!")
+}
+
+// Delegate my vote to other
+const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
+    // governance token contract
+    const governanceToken = await ethers.getContractAt(
+        "GovernanceToken", 
+        governanceTokenAddress
+    );
+    const tx = await governanceToken.delegate(delegatedAccount)
+    await tx.wait(1);
+    // ERC20Votes.sol - we look at number _checkpoints we can see how many checkpoints that account actually has.. numCheckpoints
+    //                - _moveVotingPower - when do voting or delegate basically call this function. 
+    console.log(`Checkpoints ${await governanceToken.numCheckpoints(delegatedAccount)}`) 
+    // result : Checkpoints 1 
+    // process : await tx.wait(1); -> This code make 1 checkpoint and console.log show it to us.
+    // If you see zero checkpoints here it means you haven't delegated correctly.
 }
 
 export default deployGovernanceToken;
