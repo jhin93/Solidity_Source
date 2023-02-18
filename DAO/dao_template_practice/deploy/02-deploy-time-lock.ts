716bdfe5eabb565e6e3d662971b47586374e22b2
jhin93 <scene1993@gmail.com>
2023-02-18 20:10:12 +0900

    40분에서 deploy실패, 에러해결중

--- DAO/dao_template_practice/deploy/02-deploy-time-lock.ts
+++ DAO/dao_template_practice/deploy/02-deploy-time-lock.ts
@@ -1,20 +1,25 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from 'hardhat-deploy/types';
-// @ts-ignore
-import { ethers } from "hardhat";
+import { MIN_DELAY } from "../helper-hardhat-config";
 
-const deployTimeLock: DeployFunction = async function (
-    hre: HardhatRuntimeEnvironment
-) {
+const deployTimeLock: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     // @ts-ignore
-    const { getNamedAccounts, deployments } = hre;
-    const { deploy, log } = deployments;
-    const { deployer } = await getNamedAccounts();
-    log("Deploying Timelock...")
-
+    const { getNamedAccounts, deployments } = hre
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    log("----------------------------------------------------")
+    log("Deploying TimeLock...")
     const timeLock = await deploy("TimeLock", {
-        from: deployer,
-        args: []
+      from: deployer,
+      /**
+       * Here we can set any address in admin role also zero address.
+       * previously In tutorial deployer has given admin role then
+       * renounced as well. in later section so we are doing the same by giving admin role to
+       * deployer and then renounced to keep the tutorial same.
+       */
+      args: [MIN_DELAY, [], []],
+      log: true,
     })
-
-};
+  };
+  
+  export default deployTimeLock
---
+++ DAO/dao_template_practice/helper-hardhat-config.ts
@@ -0,0 +1,1 @@
+export const MIN_DELAY = 3600 // 1 hour - after a vote passes, you have 1 hour before you can enact
