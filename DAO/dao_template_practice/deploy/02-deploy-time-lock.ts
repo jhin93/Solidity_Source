d481301593c2d3c0b0bdce821af89e5db547bb67
jhin93 <scene1993@gmail.com>
2023-02-22 20:54:20 +0900

    2/22

--- DAO/dao_template_practice/deploy/02-deploy-time-lock.ts
+++ DAO/dao_template_practice/deploy/02-deploy-time-lock.ts
@@ -2,24 +2,26 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from 'hardhat-deploy/types';
 import { MIN_DELAY } from "../helper-hardhat-config";
 
-const deployTimeLock: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    // @ts-ignore
-    const { getNamedAccounts, deployments } = hre
-    const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts()
-    log("----------------------------------------------------")
-    log("Deploying TimeLock...")
-    const timeLock = await deploy("TimeLock", {
-      from: deployer,
-      /**
-       * Here we can set any address in admin role also zero address.
-       * previously In tutorial deployer has given admin role then
-       * renounced as well. in later section so we are doing the same by giving admin role to
-       * deployer and then renounced to keep the tutorial same.
-       */
-      args: [MIN_DELAY, [], [], deployer],
-      log: true,
-    })
+const deployTimeLock: DeployFunction = async function (
+      hre: HardhatRuntimeEnvironment
+  ) {
+      // @ts-ignore
+      const { getNamedAccounts, deployments } = hre
+      const { deploy, log } = deployments
+      const { deployer } = await getNamedAccounts()
+      log("----------------------------------------------------")
+      log("Deploying TimeLock...")
+      const timeLock = await deploy("TimeLock", {
+        from: deployer,
+        /**
+         * Here we can set any address in admin role also zero address.
+         * previously In tutorial deployer has given admin role then
+         * renounced as well. in later section so we are doing the same by giving admin role to
+         * deployer and then renounced to keep the tutorial same.
+         */
+        args: [MIN_DELAY, [], [], deployer],
+        log: true,
+      })
   };
   
   export default deployTimeLock
